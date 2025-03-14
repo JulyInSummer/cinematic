44e151726231b296f66d0670855e3938ad139c0c
JulyInSummer <azimjanovbogdan@gmail.com>
2025-03-14 17:15:16 +0500

    ref: add storage interface for CRUD operation on movies

---
+++ internal/app/storage/storage.go
@@ -0,0 +1,14 @@
+package storage
+
+import (
+	"context"
+	"github.com/JulyInSummer/cinematic/internal/app/storage/postgres/models"
+)
+
+type MoviesI interface {
+	Create(ctx context.Context, movie models.Movie) error
+	GetAll(ctx context.Context) ([]models.Movie, error)
+	GetByID(ctx context.Context, id int) (*models.Movie, error)
+	Update(ctx context.Context, movie models.Movie) (*models.Movie, error)
+	Delete(ctx context.Context, id int) error
+}
